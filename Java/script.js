579378866d978c7f5c6f03398312f266c7160319
Zeldamaster21 <dfelipe@hawk.iit.edu>
2025-12-10 18:47:16 -0600

    Update script.js

--- Java/script.js
+++ Java/script.js
@@ -1,3 +1,11 @@
+const hamMenu = document.querySelector(".ham-menu");
+
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
 // document.addEventListener("DOMContentLoaded", () => {
 //   const calendarBox = document.getElementById("calendar-box");
 
